5889381d825d717f03621af6a6b13b6d00da4f93
PadmaPriya Sundaram <psundaram@anpi.com>
2015-04-24 16:07:32 +0530

    Query change

--- src/main/java/com/anpi/app/util/CDRDBOperations.java
+++ src/main/java/com/anpi/app/util/CDRDBOperations.java
@@ -12,8 +12,7 @@ import com.anpi.app.domain.TrafficSummary;
 
 public class CDRDBOperations {
 
-
-	public  TrafficLoadBean getTrafficLoad(String stEnterPriseId, long fromDateMilli, long toDateMilli, long increment) throws Exception {
+	public TrafficLoadBean getTrafficLoad(String stEnterPriseId, long fromDateMilli, long toDateMilli, long increment) throws Exception {
 		List<TrafficSummary> trafficSummaries = new ArrayList<TrafficSummary>();
 		long startTime = new Date().getTime();
 		System.out.println("sEnterpriseId :" + stEnterPriseId + ", fromDateMilli" + fromDateMilli + ",toDateMilli" + toDateMilli + ",increment:" + increment);
@@ -29,7 +28,10 @@ public class CDRDBOperations {
 			if (fromDateMilli < toDateMilli) {
 				// Connect to database
 				conn = cdrConnector.getDBConnection();
-				String stInsert = "select starttime,endtime,SUM(CASE WHEN a.direction = 'In' THEN 1 ELSE 2 END) as directiontype from (SELECT direction,starttime,endtime,fromnumber,tonumber,duration,SourceRecordId FROM tbCallLog where enterpriseid =? and (starttime>? and endtime<?)) as a group by a.starttime,a.endtime,a.fromnumber,a.tonumber,a.duration,a.SourceRecordId;";
+				String stInsert = "select A.starttime,A.endtime,SUM(CASE WHEN direction = 'In' THEN 1 ELSE 2 END) as directiontype from (SELECT direction,starttime,endtime,fromnumber,tonumber,duration FROM tbCallLog where enterpriseid ='100926'  and (starttime>1379611843479 and endtime<1379621846482) group by  direction,starttime,endtime,fromnumber,tonumber,duration ) as A  group by  starttime,endtime,fromnumber,tonumber,duration"; 
+//						"select starttime,endtime,SUM(CASE WHEN direction = 'In' THEN 1 ELSE 2 END) as directiontype from (select * from  (SELECT direction,starttime,endtime,fromnumber,tonumber,duration FROM tbCallLog where enterpriseid =?  and (starttime>? and endtime<?)) as directions  group by  direction,starttime,endtime,fromnumber,tonumber,duration ) as cdr  group by  starttime,endtime,fromnumber,tonumber,duration";
+				// String stInsert =
+				// "select starttime,endtime,SUM(CASE WHEN a.direction = 'In' THEN 1 ELSE 2 END) as directiontype from (SELECT direction,starttime,endtime,fromnumber,tonumber,duration FROM tbCallLog where enterpriseid =? and (starttime>? and endtime<?)) as a group by a.starttime,a.endtime,a.fromnumber,a.tonumber,a.duration;";
 				pstmt = conn.prepareStatement(stInsert);
 				pstmt.setString(1, stEnterPriseId);
 				pstmt.setLong(2, fromDateMilli);
@@ -44,7 +46,7 @@ public class CDRDBOperations {
 					trafficSummaries.add(trafficSummary);
 					System.out.println("startTime:" + trafficSummary.getStartTime() + " , directionType:" + trafficSummary.getDirection() + " ,endtime:" + trafficSummary.getEndtTime());
 				}
-				System.out.println("TrafficSummary Size :"+trafficSummaries.size());
+				System.out.println("TrafficSummary Size :" + trafficSummaries.size());
 				// Calculate inbound,outbound, intercom values based on interval
 				while (fromDateMilli < toDateMilli) {
 					long tempTimeMilli = fromDateMilli + increment;
@@ -52,7 +54,9 @@ public class CDRDBOperations {
 					int inLogCount = 0;
 					int outLogCount = 0;
 					int interCount = 0;
-					// Checks for the existence of time interval with the db values. If values exist, then check for the direction and increment the respective counter by 1
+					// Checks for the existence of time interval with the db
+					// values. If values exist, then check for the direction and
+					// increment the respective counter by 1
 					for (int i = 0; i < trafficSummaries.size(); i++) {
 						TrafficSummary trafficSummary = (TrafficSummary) trafficSummaries.get(i);
 						direction = trafficSummary.getDirection();
@@ -76,12 +80,13 @@ public class CDRDBOperations {
 							}
 						}
 					}
-					// Set the inbound,outbound and intercom based on the count value.
+					// Set the inbound,outbound and intercom based on the count
+					// value.
 					inBound.add("[" + (new Date(fromDateMilli)).getTime() + "," + inLogCount + "]");
 					outBound.add("[" + (new Date(fromDateMilli)).getTime() + "," + outLogCount + "]");
 					interCom.add("[" + (new Date(fromDateMilli)).getTime() + "," + interCount + "]");
 					fromDateMilli = tempTimeMilli;
-					
+
 				}
 				loadBean = new TrafficLoadBean();
 				loadBean.setInBoundLoad(inBound.toString());
@@ -91,14 +96,12 @@ public class CDRDBOperations {
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			// Close db connection
 			cdrConnector.closeConnection(conn);
 		}
 		System.out.println("TotalTime:" + (new Date().getTime() - startTime));
 		return loadBean;
 	}
-
-	
 
 }
